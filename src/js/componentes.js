e1fe9d01be86868430de244a0027cb85a1535ae2
Gabriel Yañez <yanez.gabriel.gustavo@gmail.com>
2020-10-13 02:27:27 -0300

    Styles and minor changes

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,8 +1,11 @@
 import '../css/componentes.css';
 
 export const saludar = (nombre) => {
-    console.log('Creando etiqueta h1, en el HTML');
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre}, ¿cómo estás?`;
-    document.body.append(h1);
+    // console.log('Creando etiqueta h1, en el HTML');
+
+    const mainContainer = document.querySelector('#main-container');
+    let miNombre = document.createElement('h3');
+    miNombre.innerText = `~ ${nombre}`;
+    miNombre.style.color = 'indigo';
+    mainContainer.append(miNombre);
 }
